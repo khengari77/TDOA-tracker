d8097ae286cc468489dc716e28a3651512efbff8
khengari77 <khengari77@gmail.com>
2026-05-18 10:39:06 +0200

    Update configuration: enable unix timestamps, set gateway name
    - Enable message_unixtimestamp for millisecond-precision
      unix timestamps in MQTT messages
    - Change Gateway_Name from 'OMG_ESP32_BLE_LORA' to 'gateway_5'
      for tracker deployment

--- main/User_config.h
+++ main/User_config.h
@@ -428,7 +428,7 @@ int lowpowermode = DEFAULT_LOW_POWER_MODE;
 #  define message_UTCtimestamp false //define true if you want messages to be timestamped in ISO8601 UTC format (e.g.: "UTCtime"="2023-12-26T19:10:20Z")
 #endif
 #ifndef message_unixtimestamp
-#  define message_unixtimestamp false //define true if you want messages to have an unix timestamp (e.g.: "unixtime"=1679015107)
+#  define message_unixtimestamp true //define true if you want messages to have an unix timestamp (e.g.: "unixtime"=1679015107)
 #endif
 
 /*-------------DEFINE YOUR OTA PARAMETERS BELOW----------------*/
